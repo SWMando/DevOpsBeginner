d53c3336f88343e356ed9022051564bd6ff8fca1
SWMando <akbarov.salim@gmail.com>
2025-03-07 01:11:51 +0200

    Changes to log-archive.sh

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ Script which shows to the admin:
 - Top 5 process by Memory Usage
 
 In order to get dynamic output, use `watch -n1 "bash server-stats.sh"`. This uses `watch` command and `-n1` option which sets the update interval to 1s. In other words, script will be running every second and the output will be updated.
+
+### 2. Bash script 'log-archive.sh'.
+
+Script which allows admin to:
+- Archive selected log file
+- Saves all archived `tar.gz` files in its own directory `/var/log/logs_archived`
+
+Because the directory is in `/var/log/logs_archived` you might need elevated priviliges to run the script.
--- log-archive.sh
+++ log-archive.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+###############################################################################
+# Please provide execute permissions for the script and run it with elevated  #
+# priviliges, or you might permission issues while creating directory for the #
+# archives                                                                    #
+###############################################################################
+
 ARCHIVE="logs_archive_$(date +%Y%m%d_%H%M%S).tar.gz"
 ARCHIVE_DIR="/var/log/logs_archived"
+
+if [ -z "$1" ]; then
+    echo "Error: No file specified to archive."
+    exit 1
+fi
 
 if [ ! -d "$ARCHIVE_DIR" ];
 then
@@ -14,7 +25,7 @@ if [ "$?" -eq 0 ];
 then
 	mv "$ARCHIVE" "$ARCHIVE_DIR"/.
 	echo -e "Log file archived successfully!\nName of the file: $ARCHIVE\nLocation of the file: $ARCHIVE_DIR/$ARCHIVE"
+else
+	echo "There was an error!"
+	exit 1
 fi
-
-
-
